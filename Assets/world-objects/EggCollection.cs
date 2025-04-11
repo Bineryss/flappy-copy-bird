eb56efb59de3e979d20c9ce12127456ae9e6a42e
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2025-04-11 03:14:11 +0200

    hide egg if collected

--- Assets/world-objects/EggCollection.cs
+++ Assets/world-objects/EggCollection.cs
@@ -2,23 +2,13 @@ using UnityEngine;
 
 public class EggCollection : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.tag.Equals("player")) return;
         Debug.Log(collision.tag);
         InventoryManager.instance.AddEgg();
         InventoryManager.instance.SaveGame();
+        gameObject.SetActive(false);
+        //TODO trigger animation
     }
 }
